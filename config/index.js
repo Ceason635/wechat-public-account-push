08d99be643d34e00be6332a6c3041bb226b0a548
Ceason635 <71577475+Ceason635@users.noreply.github.com>
2022-09-20 02:24:44 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -64,7 +64,7 @@ export const config = {
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: "o97vH5ltTMO8ytLDu_SUtfNHW9Uw",
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: "ouLRudaPGCqZwTtp2mWAEmAKBTlqKAYq_V0VTYUo1LI	",
+      useTemplateId: "52LPW5yzUUPeD44lzu1PrTZinYaPRit5HWdFfZWiK50	",
       // 所在省份
       province: "四川",
       // 所在城市
@@ -100,7 +100,7 @@ export const config = {
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: "o97vH5ltTMO8ytLDu_SUtfNHW9Uw",
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: "ouLRudaPGCqZwTtp2mWAEmAKBTlqKAYq_V0VTYUo1LI	",
+      useTemplateId: "52LPW5yzUUPeD44lzu1PrTZinYaPRit5HWdFfZWiK50	",
       // 所在省份
       province: "广东",
       // 所在城市
