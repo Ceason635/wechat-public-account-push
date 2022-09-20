ba571da368430c309bc030975eb0ddde060a719e
Ceason635 <71577475+Ceason635@users.noreply.github.com>
2022-09-20 08:36:01 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -62,7 +62,7 @@ export const config = {
       // 想要发送的人的名字
       name: "小学生",
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: "o97vH5ltTMO8ytLDu_SUtfNHW9Uw",
+      id: "o97vH5rsEP87KaXC7EbmWJoI-RK4",
       // 你想对他发送的模板消息的模板ID
       useTemplateId: "RkFtCGfcKBEunWPJb3ZmEmCnRfSHGeZbzERVb73_4ZI",
       // 所在省份
