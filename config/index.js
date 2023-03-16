b8f5ad043e3b0fec8cc39c70d222bf7019571237
Ceason635 <71577475+Ceason635@users.noreply.github.com>
2023-03-16 12:19:59 +0800

    ..

--- config/index.js
+++ config/index.js
@@ -36,9 +36,9 @@ export const config = {
     // 朋友圈文案
     momentCopyrighting: true,
     // 毒鸡汤
-    poisonChickenSoup: true,
+    poisonChickenSoup: false,
     // 古诗古文
-    poetry: true,
+    poetry: false,
 
     /** 星座运势 */
     horoscope: true,
@@ -65,7 +65,7 @@ export const config = {
       id: "o97vH5rsEP87KaXC7EbmWJoI-RK4",
 //       id: "o97vH5ltTMO8ytLDu_SUtfNHW9Uw",
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: "fy61ixgAjd49Zr56jy5jtGrIuemXNhx0IMsQ4cnJzoE",
+      useTemplateId: "mYaGbNerFKlliPJuEHtrgyaIx6z9qP4lJeYtdllWUGs",
       // 所在省份
       province: "四川",
       // 所在城市
@@ -101,7 +101,7 @@ export const config = {
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: "o97vH5ltTMO8ytLDu_SUtfNHW9Uw",
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: "fy61ixgAjd49Zr56jy5jtGrIuemXNhx0IMsQ4cnJzoE",
+      useTemplateId: "mYaGbNerFKlliPJuEHtrgyaIx6z9qP4lJeYtdllWUGs",
       // 所在省份
       province: "广东",
       // 所在城市
