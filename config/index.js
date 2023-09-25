34d43c2b74833010ca11a0187bbf4dac8a7a5afa
Ceason635 <71577475+Ceason635@users.noreply.github.com>
2023-09-25 10:17:18 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -132,42 +132,7 @@ export const config = {
         // 退伍日
 //         {"keyword": "ex_day", date: "2022-09-10"},
       ]
-    },
-    {
-      // 想要发送的人的名字
-      name: "徐秋树",
-      // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: "o97vH5key_GC2GYM6vFX7gWJOBh4",
-      // 你想对他发送的模板消息的模板ID
-      useTemplateId: "EEvALt_CWFtMNpweYrbLilcHfYpWozmLd4PxzDBYI8M",
-      // 所在省份
-      province: "四川",
-      // 所在城市
-      city: "成都",
-      // 新历生日, 仅用作获取星座运势, 格式必须
-      horoscopeDate: '',
-      // 获取什么时候的星座运势，可选：['今日', '明日', '本周', '本月', '今年'], 留空则随机
-      horoscopeDateType: '今日',
-      // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
-      openUrl: "http://authserver.cwnu.edu.cn/authserver/login?service=http%3A%2F%2Fehall.cwnu.edu.cn%2Flogin%3Fservice%3Dhttp%3A%2F%2Fehall.cwnu.edu.cn%2Fnew%2Findex.html",
-      // 专属节日提醒，如果你在这里填写了节日提醒，就不会执行FESTIVALS的提醒了, 和FESTIVALS的配置方法相同，可以往下查看，我这里就不重复写了
-      festivals: [
-//         注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
-//         {"type": "*生日", "name": "小学生", "year": "2003", "date": "03-02"},
-
-        // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
-
-      ],
-      // 专属纪念日/倒数日，如果你在这里填写了纪念日/倒数日，就不会计算CUSTOMIZED_DATE_LIST的日子了, 和CUSTOMIZED_DATE_LIST的配置方法相同，可以往下查看，我这里就不重复写了
-      customizedDateList: [
-        // 在一起的日子
-//         {"keyword": "love_day", date: "2021-12-31"},
-        // 结婚纪念日
-//         {"keyword": "marry_day", date: "2022-09-09"},
-        // 退伍日
-//         {"keyword": "ex_day", date: "2022-09-10"},
-      ]
-    },
+    }
     // 你可以不断按格式往下增加
     // ...
   ],
